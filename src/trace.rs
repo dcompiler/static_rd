6e77091cc4a64b351c00be75e6517eb2b362f169
C Ding <cding@cs.rochester.edu>
2023-06-17 15:06:50 -0400

    trace func updated

--- src/trace.rs
+++ src/trace.rs
@@ -1,5 +1,5 @@
-use v3pluss::loop_tree::*;
-use v3pluss::arybase::set_arybase;
+use dace::ast::{Node, Stmt, LoopBound, AryRef};
+use dace::arybase::set_arybase;
 use lru_stack::LRUStack;
 use hist::Hist;
 use std::rc::Rc;
@@ -15,17 +15,17 @@ fn access2addr(ary_ref: &AryRef, ivec: &Vec<i32>) -> usize {
     return ary_ref.base.unwrap() + offset;
 }
 
-fn trace_rec(code: &Rc<LoopTNode>, ivec: &Vec<i32>, sim: &mut LRUStack<usize>, hist: &mut Hist) {
+fn trace_rec(code: &Rc<Node>, ivec: &Vec<i32>, sim: &mut LRUStack<usize>, hist: &mut Hist) {
     match &code.stmt {
 	Stmt::Ref(ary_ref) => {let addr = access2addr(&ary_ref, &ivec);
-			       let rd = sim.rec_access(addr);
+			       let rd = sim.rec_access_impl(addr);
 			       hist.add_dist(rd);},
 	Stmt::Loop(aloop) => {
 	    if let LoopBound::Fixed(lb) = aloop.lb {
 		if let LoopBound::Fixed(ub) = aloop.ub {
 		    (lb..ub).into_iter().for_each(
 			|i| {
-			aloop.body.borrow().iter().for_each(
+			aloop.body.iter().for_each(
 			    |stmt| {
 				let mut myvec = ivec.clone();
 				myvec.push(i);
@@ -34,11 +34,12 @@ fn trace_rec(code: &Rc<LoopTNode>, ivec: &Vec<i32>, sim: &mut LRUStack<usize>, h
 		else {panic!("dynamic loop upper bound is not supported")}
 	    }
 	    else {panic!("dynamic loop lower bound is not supported")}
-	}
+	},
+	Stmt::Block(blk) => blk.iter().for_each( |s| trace_rec(s, &ivec.clone(), sim, hist) )
     }
 }
 
-pub fn trace(code: &mut Rc<LoopTNode>) -> Hist {
+pub fn trace(code: &mut Rc<Node>) -> Hist {
     let mut sim = LRUStack::<usize>::new();
     let mut hist = Hist::new();
     set_arybase( code );
@@ -53,7 +54,7 @@ mod test {
 
     #[test]
     fn test_access2addr() {
-	let mut aij_node = LoopTNode::new_ref("x", vec![10,10],
+	let mut aij_node = Node::new_ref("x", vec![10,10],
 					      |ij| vec![ij[0] as usize, ij[1] as usize]);
 	let mutable = unsafe{ Rc::get_mut_unchecked( &mut aij_node ) };
 	*mutable.ref_only_mut_ref( |a| &mut a.base ).unwrap() = Some(0);
@@ -67,10 +68,10 @@ mod test {
     fn loop_a_i() {
 
         // i = 0, n { a[i] }
-        let aref = LoopTNode::new_ref("A", vec![10],
+        let mut aref = Node::new_ref("A", vec![10],
 				      |i| vec![i[0] as usize]);
-        let mut aloop = LoopTNode::new_single_loop("i", 0, 10);
-        LoopTNode::extend_loop_body(&aloop, &aref);
+        let mut aloop = Node::new_single_loop("i", 0, 10);
+        Node::extend_loop_body(&mut aloop, &mut aref);
 
 	let hist = trace(&mut aloop);
 	assert_eq!(hist.to_vec()[0], (None, 10));
@@ -81,9 +82,9 @@ mod test {
     fn loop_a_0() {
 
         // i = 0, n { a[0] }
-        let aref = LoopTNode::new_ref("A", vec![1], |_| vec![0]);
-        let mut aloop = LoopTNode::new_single_loop("i", 0, 10);
-        LoopTNode::extend_loop_body(&aloop, &aref);
+        let mut aref = Node::new_ref("A", vec![1], |_| vec![0]);
+        let mut aloop = Node::new_single_loop("i", 0, 10);
+        Node::extend_loop_body(&mut aloop, &mut aref);
 
 	let hist = trace(&mut aloop);
 	assert_eq!(hist.to_vec()[0], (Some(1), 9));
